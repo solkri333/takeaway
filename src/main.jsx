5106e56b90b01f69725885f56d40322035037ca9
solkri333 <160307302+solkri333@users.noreply.github.com>
2025-02-15 21:31:41 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import BlogPage from './blog.jsx';
-import Page from "./page.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
     <Router>
       <Routes>
         <Route path='/' element={<BlogPage />}/>
-          <Route path='/content/:id' element={<Page />}/>
       </Routes>
     </Router>
-  </StrictMode>,
 )
