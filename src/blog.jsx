439d4bcb68c47ffd6c373c03c2cea3dcbdf8dfde
solkri333 <160307302+solkri333@users.noreply.github.com>
2025-02-17 23:37:09 +0530

    Update blog.jsx

--- src/blog.jsx
+++ src/blog.jsx
@@ -18,6 +18,7 @@ fetch(f)
 },[]);
 
   return (
+          <>
     <div className="min-h-screen bg-gradient-to-br from-blue-500 to-purple-700 text-white" id="blog-page">
       <header className="flex justify-between items-center p-4 bg-opacity-20 backdrop-blur-lg shadow-lg fixed top-0 w-full" id="header">
         <h1 className="text-xl font-bold" id="site-title">Late Night <div className="read">Read</div></h1>
@@ -33,6 +34,8 @@ fetch(f)
         dangerouslySetInnerHTML={{ __html: text }}
       />
 
+      <footer style={{ height: "50px" }}></footer>
+    </div>
             <a className="unique-class" href="https://txxx.tube/videos/20660585/250213-i4/?promo=47026" target="_blank">Click</a>
             <a className="unique-class" href="https://videovoyeurhit.com/videos/360947/two-european-female-slaves-and-publi-with-steve-holmes-antonio-ross-and-lyen-parker/?promo=47026" target="_blank">Click</a>
             <a className="unique-class" href="https://videomanysex.com/video/3147939/poke-her/?promo=47026" target="_blank">Click</a>
@@ -42,7 +45,6 @@ fetch(f)
             <a className="unique-class" href="https://videohotmovs.com/videos/12843561/we-cant-get-enough-of-aurora-jolies-big-asses2/?promo=47026" target="_blank">Click</a>
             <a className="unique-class" href="https://videovjav.com/videos/870909/japanese-261ara-553-limited-scene/?promo=47026" target="_blank">Click</a>
             <a className="unique-class" href="https://videotubepornclassic.com/videos/1204357/all-the-pussy-you-want-to-make-you-enjoy-5-spoken/?promo=47026" target="_blank">Click</a>
-      <footer style={{ height: "50px" }}></footer>
-    </div>
+          </>
   );
 }
