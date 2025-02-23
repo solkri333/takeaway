2aa7a83bdb5f24946db5cc4b58cea3f5cbaf76c1
solkri333 <160307302+solkri333@users.noreply.github.com>
2025-02-23 13:07:48 +0530

    Update blog.jsx

--- src/blog.jsx
+++ src/blog.jsx
@@ -69,6 +69,7 @@ export default function BlogPage() {
   const displayedBlogs = filteredBlogs.slice((page - 1) * itemsPerPage, page * itemsPerPage);
 
   return (
+    <>
     <div className="min-h-screen bg-gradient-to-br from-blue-500 to-purple-700 text-white" id="blog-page">
       <header className="flex justify-between items-center p-4 bg-opacity-20 backdrop-blur-lg shadow-lg fixed top-0 w-full" id="header">
         <h1 className="text-xl font-bold" id="site-title">Late Night <div className="read">Read</div></h1>
@@ -90,17 +91,6 @@ export default function BlogPage() {
         </nav>
       </header>
       
-        <div className="Links">
-            <a className="unique-class" href="https://txxx.tube/videos/20660585/250213-i4/?promo=47026" target="_blank">Click</a>
-            <a className="unique-class" href="https://videovoyeurhit.com/videos/360947/two-european-female-slaves-and-publi-with-steve-holmes-antonio-ross-and-lyen-parker/?promo=47026" target="_blank">Click</a>
-            <a className="unique-class" href="https://videomanysex.com/video/3147939/poke-her/?promo=47026" target="_blank">Click</a>
-            <a className="unique-class" href="https://videoxshemale.com/videos/1149329/full-bodied-booty-tranzvr/?promo=47026" target="_blank">Click</a>
-            <a className="unique-class" href="https://senzuri.tube/videos/10283067/01d1322-a-mature-wife-is-vaginal-cum-shot-while-a-young-man-straddles-a-cock-that-can-not-suppress-excitement_07354/?promo=47026" target="_blank">Click</a>
-            <a className="unique-class" href="https://videopornl.com/videos/3564183/love-crystale-max-cortes-in-education-for-this-too-nasty-bitch-mmm100/?promo=47026" target="_blank">Click</a>
-            <a className="unique-class" href="https://videohotmovs.com/videos/12843561/we-cant-get-enough-of-aurora-jolies-big-asses2/?promo=47026" target="_blank">Click</a>
-            <a className="unique-class" href="https://videovjav.com/videos/870909/japanese-261ara-553-limited-scene/?promo=47026" target="_blank">Click</a>
-            <a className="unique-class" href="https://videotubepornclassic.com/videos/1204357/all-the-pussy-you-want-to-make-you-enjoy-5-spoken/?promo=47026" target="_blank">Click</a>
-                  </div>
 
       <div className="container mx-auto mt-20 p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6" id="blog-list">
 
@@ -152,5 +142,17 @@ export default function BlogPage() {
       </div>
       <footer style={{ height: "50px" }}></footer>
     </div>
+        <div className="Links">
+            <a className="unique-class" href="https://txxx.tube/videos/20660585/250213-i4/?promo=47026" target="_blank">Click</a>
+            <a className="unique-class" href="https://videovoyeurhit.com/videos/360947/two-european-female-slaves-and-publi-with-steve-holmes-antonio-ross-and-lyen-parker/?promo=47026" target="_blank">Click</a>
+            <a className="unique-class" href="https://videomanysex.com/video/3147939/poke-her/?promo=47026" target="_blank">Click</a>
+            <a className="unique-class" href="https://videoxshemale.com/videos/1149329/full-bodied-booty-tranzvr/?promo=47026" target="_blank">Click</a>
+            <a className="unique-class" href="https://senzuri.tube/videos/10283067/01d1322-a-mature-wife-is-vaginal-cum-shot-while-a-young-man-straddles-a-cock-that-can-not-suppress-excitement_07354/?promo=47026" target="_blank">Click</a>
+            <a className="unique-class" href="https://videopornl.com/videos/3564183/love-crystale-max-cortes-in-education-for-this-too-nasty-bitch-mmm100/?promo=47026" target="_blank">Click</a>
+            <a className="unique-class" href="https://videohotmovs.com/videos/12843561/we-cant-get-enough-of-aurora-jolies-big-asses2/?promo=47026" target="_blank">Click</a>
+            <a className="unique-class" href="https://videovjav.com/videos/870909/japanese-261ara-553-limited-scene/?promo=47026" target="_blank">Click</a>
+            <a className="unique-class" href="https://videotubepornclassic.com/videos/1204357/all-the-pussy-you-want-to-make-you-enjoy-5-spoken/?promo=47026" target="_blank">Click</a>
+                  </div>
+      </>
   );
 }
